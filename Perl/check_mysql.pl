edc5c025dca8310c795b25229cbbe06ca49f8469
akavjik <akavjik@gmail.com>
2017-10-16 01:35:33 +0300

    edit check_mysql.pl

--- Perl/check_mysql.pl
+++ Perl/check_mysql.pl
@@ -1,18 +1,23 @@
-#!/bin/perl
+#!/usr/bin/perl
+use Time::Local;
 use DBI;
+$now_string = localtime;
+
 $db='mysql';
 $host='localhost';
 $user='user';
 $pwd='pwd';
-
- $dbh = DBI->connect('dbi:mysql:$db:$host,$user,$pwd)
-   or die "Connection Error: $DBI::errstr\n";
+$log="/var/log/!_check.log";
+open(STDERR, '>>', $log) or die "Can't open log";
+$dbh = DBI->connect("dbi:mysql:$db:$host",$user,$pwd)
+   or die $now_string." -> Connection Error: $DBI::errstr\n";
  $sql = "SHOW GLOBAL STATUS like 'Uptime';";
  $sth = $dbh->prepare($sql);
  $sth->execute
    or die "SQL Error: $DBI::errstr\n";
- while (@row = $sth->fetchrow_array) {
-#   print "@row\n";
-   $cmd="echo '".join(" ",@row)."'>>/var/log/!mysql_check.log";
-   $out=`$cmd`;
- }
+while (@row = $sth->fetchrow_array) {
+#    print "@row\n";
+    $cmd="echo '".$now_string."->".join(" ",@row)."'>>$log";
+    $out=`$cmd`;
+}
+
